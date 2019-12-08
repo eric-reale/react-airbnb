d4e625f675c44575e9b62d081636cd34fea98cea
Eric Reale <ecr5083@gmail.com>
2019-12-08 13:58:57 +0700

    set up active click on flat

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 
 class Flat extends Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
-    console.log({props})
+
+    this.state = {
+      clicked: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    });
   }
 
   render() {
     return (
-      <div className="card" style={{ backgroundImage: `url(${this.props.image})` }}>
-        {this.props.name} {this.props.price}
+      <div className={this.state.clicked ? 'card active' : 'card'} onClick={this.handleClick} style={{ backgroundImage: `url(${this.props.image})` }}>
+        <div className="card-category">
+          {this.props.price} {this.props.price_currency}
+        </div>
+        <div className="card-description">
+          <h2>{this.props.name}</h2>
+        </div>
+        <a class="card-link" href="#"></a>
       </div>
     );
   }
